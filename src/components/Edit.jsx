3d6ab8889f2c893128c60523994965d764696aea
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2023-07-15 17:19:43 +0200

    add ./src/components/Button.css ./src/components/Controls.css ./src/components/Edit.jsx ./src/assets/bold-solid.svg
    add components Button and Controls their .css
    
    add bolden icon
    
    reduce edit preview border radius

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,6 +1,33 @@
-import React from "react";
+import { React, useRef } from "react";
+import boldIconPath from "../assets/bold-solid.svg";
+
+import "./Controls.css";
+import "./Button.css";
+
+function Button({ text, imagePath }) {
+    // get reference to current object
+    const ref = useRef(null);
+    const backgroundSize = "1rem";
+
+    const style = {
+        backgroundImage: `url(${imagePath}`,
+        backgroundSize: backgroundSize,
+        /* move the text below the icon, only if there is text */
+        paddingTop: text === undefined ? 0 : `calc(${backgroundSize} + 0.5rem)`,
+    };
+    return (
+        <button className="button" style={style} ref={ref}>
+            {text}
+        </button>
+    );
+}
+
+function Controls({ children }) {
+    return <section id="controls">{children}</section>;
+}
 
 const Edit = (props) => {
+    function onFocus(event) {}
     const {
         id,
         headerId,
@@ -19,6 +46,9 @@ const Edit = (props) => {
             <h2 id={headerId} style={headerStyle}>
                 Editor
             </h2>
+            <Controls>
+                <Button imagePath={boldIconPath} text="Bold" />
+            </Controls>
             <textarea
                 id={textareaId}
                 style={textareaStyle}
